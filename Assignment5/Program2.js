a25d75a807c237f284afa3f2c2c4a0b94bcf5472
Virat Tripathi <virat.tripathi@virattripathiss437.successivetech.com>
2025-06-12 21:53:18 +0530

    Assignment5 updated

--- Assignment5/Program2.js
+++ Assignment5/Program2.js
@@ -1,11 +1,31 @@
 // 2. Write a program to sort an array of object on the basis of age ({name:"John", age:26})
 
-const arr = [{name : "Virat",age : 25},{name : "Divyanshu",age : 29},{name : "Parth" , age : 23}];
-//object arr to store the objects
+function sortByAge(arr , order )
+{
+    if(!Array.isArray(arr))
+    {
+        throw new Error("Input must be an aray");
+    }
+    return arr.sort((a,b)=>{
+        if(order=="asc")
+        {
+            return a.age - b.age ;
+        }
+        else if(order =="desc")
+        {
+            return b.age-a.age;
+        }
+        else{
+            throw new Error("Order must be 'asc' or 'desc' ");
+        }
+    })
+}
+
+const people = [{name:"Virat",age:25},{name:"Mannat",age:22},{name:"Aditi",age:24}];
+console.log("Ascending");
+console.log(sortByAge(people,"asc"));
+console.log("Descending")
+console.log(sortByAge(people,"desc"));
 
 
-arr.sort((a,b)=>a.age - b.age);
-
-//sort function to sort the ages of the stored objects 
-
-console.log(arr);
+//Changes done 
