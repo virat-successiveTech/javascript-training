4f5e92d268ab44a966f3ecdbfb913948aab2387c
Virat Tripathi <virat.tripathi@virattripathiss437.successivetech.com>
2025-06-13 16:46:39 +0530

    Changes done

--- Assignment5/Program2.js
+++ Assignment5/Program2.js
@@ -6,26 +6,46 @@ function sortByAge(arr , order )
     {
         throw new Error("Input must be an aray");
     }
-    return arr.sort((a,b)=>{
+    
         if(order=="asc")
         {
-            return a.age - b.age ;
+            
+            for (let i = 0; i<arr.length - 1; i++){
+                for(let j = 0; j< arr.length - 1 - i; j++){
+                    if (arr[j].age > arr[j+1].age){
+                        let temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            return arr;
         }
         else if(order =="desc")
         {
-            return b.age-a.age;
+            console.log("here");
+            for (let i = 0; i<arr.length - 1; i++){
+                for(let j = 0; j< arr.length - 1 - i; j++){
+                    if (arr[j].age < arr[j+1].age){
+                        let temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            return arr;
         }
         else{
             throw new Error("Order must be 'asc' or 'desc' ");
-        }
-    })
-}
+        }console.log("here");
+    }
 
-const people = [{name:"Virat",age:25},{name:"Mannat",age:22},{name:"Aditi",age:24}];
+
+const arr = [{name:"Virat",age:25},{name:"Mannat",age:22},{name:"Aditi",age:24}];
 console.log("Ascending");
-console.log(sortByAge(people,"asc"));
+console.log(sortByAge(arr,"asc"));
 console.log("Descending")
-console.log(sortByAge(people,"desc"));
+console.log(sortByAge(arr,"desc"));
 
 
 //Changes done 
