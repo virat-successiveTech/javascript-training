a40ca06c767c0406573d1e9c7912cfb94fd55531
Virat Tripathi <virat.tripathi@virattripathiss437.successivetech.com>
2025-06-15 19:24:36 +0530

    Changes done

--- Assignment7/Program8.js
+++ Assignment7/Program8.js
@@ -1,3 +1,5 @@
+// 8. Create a function that fetches data from multiple APIs in parallel and then performs some operation on the combined data, using async/await
+
 async function fetchData(url) {
     const response = await fetch(url);
     if(!response.ok)
